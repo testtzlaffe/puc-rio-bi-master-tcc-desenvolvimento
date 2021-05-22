da24213dd75d028fc8e37b43d5dc54a19fbc27bc
Testtzlaffe <50345995+testtzlaffe@users.noreply.github.com>
2021-05-22 16:42:47 -0300

    Update download.py

--- script-download-revistas/download.py
+++ script-download-revistas/download.py
@@ -10,7 +10,7 @@ semanas = int(dias / 7)
 numero_da_revista_atual = 'RM' + str(numero_da_revista_no_dia_0 + semanas)
 
 url = 'http://revistas.inpi.gov.br/txt/' + numero_da_revista_atual + '.zip'
-path = 'D:\\shared-projects\\BI-Master\\TCC\\revistas-zipadas\\'
+path = '<DIGITE O CAMINHO DA PASTA DE DESTINO DOS ARQUIVOS BAIXADOS>'
 
 r = requests.get(url)
 open(path + numero_da_revista_atual + '.zip', 'wb').write(r.content)
