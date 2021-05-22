beab911dcf555999a9e528a180e2c1c60ad6de82
Testtzlaffe <50345995+testtzlaffe@users.noreply.github.com>
2021-05-22 16:31:32 -0300

    Update download.py

--- script-download-revistas/download.py
+++ script-download-revistas/download.py
@@ -1,1 +1,17 @@
-teste
+import requests
+from datetime import date
+
+dia_0 = date(2021,5,25)
+numero_da_revista_no_dia_0 = 2629
+hoje = date.today()
+delta = hoje - dia_0
+dias = delta.days
+semanas = int(dias / 7)
+numero_da_revista_atual = str(numero_da_revista_no_dia_0 + semanas)
+
+url = 'http://revistas.inpi.gov.br/txt/RM' + numero_da_revista_atual + '.zip'
+path = 'D:\\shared-projects\\BI-Master\\TCC\\revistas-zipadas\\RM'
+
+r = requests.get(url)
+open(path + numero_da_revista_atual + '.zip', 'wb').write(r.content)
+
